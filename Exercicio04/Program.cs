62d551c44fab14ca61025561bca6cee35d08017d
Lucas Fidelis <134640628+fidelisluc4s@users.noreply.github.com>
2023-09-20 23:43:11 -0300

    exercicio metodo recursivo que soma os numero do intervalo e que ache os q sao divididos por 2 e o 3 ao mesmo tempo

---
+++ Exercicio04/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/*Faça um procedimento que receba um número n inteiro e positivo. O
+procedimento deve imprimir todos os números do intervalo entre 0 e n que são
+divisíveis por 2 e por 3 (simultaneamente). Restrição: não utilize comandos de
+repetição.*/
+
+class Program{
+    static void Main(string[] args){        
+
+        somaIntervalo(7,1);
+
+    }
+    static void somaIntervalo(int numero, int i){
+
+        if(i<=numero){
+            if(i%2 ==0 && i %3==0){
+                Console.WriteLine(i);
+            }
+            somaIntervalo(numero, i+1);    
+        }
+    }
+}
