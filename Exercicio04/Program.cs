8069fe58ada59f0c2b0fee8939b930a1654f4794
Lucas Fidelis <134640628+fidelisluc4s@users.noreply.github.com>
2023-09-22 17:08:11 -0300

    Agradecimento ao Marcos-Andrade22 pela ajuda tmj

--- Exercicio04/Program.cs
+++ Exercicio04/Program.cs
@@ -1,25 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+/*Questão 1) Faça uma função recursiva que receba um número inteiro por parâmetro e retorne true se o número 
+for um número perfeito ou false caso contrário. Um número perfeito é um número natural para o qual a soma de 
+todos os seus divisores naturais é igual ao próprio número. Esta função não pode utilizar comandos de repetição.*/
 
-/*Faça um procedimento que receba um número n inteiro e positivo. O
-procedimento deve imprimir todos os números do intervalo entre 0 e n que são
-divisíveis por 2 e por 3 (simultaneamente). Restrição: não utilize comandos de
-repetição.*/
+
 
 class Program{
-    static void Main(string[] args){        
-
-        somaIntervalo(7,1);
-
+    static void Main(string[] args){
+        int check = numeroPerfeito(6,1,0);
+        Console.WriteLine(check);
+        
     }
-    static void somaIntervalo(int numero, int i){
-
-        if(i<=numero){
-            if(i%2 ==0 && i %3==0){
-                Console.WriteLine(i);
+    static int numeroPerfeito(int numero, int i, int somaDivisores){
+        if (i>=numero)
+        {
+            if (somaDivisores==numero){
+                return numero;
+            }else{
+                return -1;
             }
-            somaIntervalo(numero, i+1);    
         }
+        if(numero%i==0){
+            somaDivisores+=i;
+        }
+        return numeroPerfeito(numero,i+1,somaDivisores);
     }
 }
---
+++ Exercicio05/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+/*Questão 2: Faça uma função que receba um vetor de números inteiros por
+parâmetro e multiplique por -1 todos os elementos negativos desse vetor. A função
+deve retornar o número de elementos negativos encontrados. Restrição: não utilize
+comandos de repetição.*/
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numero = new int[4] { -3, 5, 4, -4 };
+        int i = 0, contador = 0;
+        System.Console.WriteLine(multipliqueNegativo(numero, i, contador));
+    }
+    static int multipliqueNegativo(int[] numero, int i, int contador)
+    {
+        if (i < numero.Length)
+        {
+            if (numero[i] < 0)
+            {
+                numero[i] *= -1;
+                return multipliqueNegativo(numero, ++i, ++contador);
+            }
+            return multipliqueNegativo(numero, ++i, contador);
+        }
+        return contador;
+    }
+}
